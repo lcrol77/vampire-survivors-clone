11ff2354b065e73b1e8cdd6a10a0bf4f836c72cf
Logan Croley <logan.croley.18@gmail.com>
2024-09-06 01:08:55 -0400

    added enemy

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-@export var movement_speed = 40.0
+@export var movement_speed = 20.0
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var player = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("left", "right", "up", "down")
+	var direction = global_position.direction_to(player.global_position)
 	if direction.x > 0: sprite_2d.flip_h = true
 	elif direction.x < 0: sprite_2d.flip_h = false
 	velocity = direction * movement_speed
 	move_and_collide(velocity * delta)
-	
