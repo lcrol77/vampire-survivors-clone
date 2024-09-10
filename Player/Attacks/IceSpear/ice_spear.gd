186af56f99590c81281bb91c1fa1d0e558458ef5
Logan Croley <logan.croley.18@gmail.com>
2024-09-10 11:40:31 -0400

    added ice spear

--- Player/Attacks/IceSpear/ice_spear.gd
+++ Player/Attacks/IceSpear/ice_spear.gd
@@ -8,4 +8,29 @@ var knock_back =100
 var attack_size = 1.0
 
 var target = Vector2.ZERO
-var angel = Vector2.ZERO
+var angle = Vector2.ZERO
+
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+
+func _ready() -> void:
+	angle = global_position.direction_to(target)
+	rotation = angle.angle() + deg_to_rad(135)
+	match level:
+		1:
+			level = 1
+			hp  = 1
+			speed = 100
+			damage = 5
+			knock_back =100
+			attack_size = 1.0
+
+func _physics_process(delta: float) -> void:
+	position += angle * speed * delta
+
+func enemy_hit(charge = 1):
+	hp -= charge
+	if hp <= 0:
+		queue_free()
+
+func _on_queue_free_timer_timeout() -> void:
+	queue_free()
--- Player/player.gd
+++ Player/player.gd
@@ -5,6 +5,25 @@ extends CharacterBody2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var walk_cycle_timer: Timer = $WalkCycleTimer
 
+# Attacks
+var ice_spear = preload("res://Player/Attacks/IceSpear/ice_spear.tscn")
+
+# AttackNodes
+@onready var ice_spear_timer:Timer = get_node("%IceSpearTimer")
+@onready var ice_spear_attack_timer:Timer = get_node("%IceSpearAttackTimer")
+
+# IceSpear
+var ice_spear_ammo = 0
+var ice_spear_base_ammo = 1
+var ice_spear_attack_speed = 1.5
+var ice_spear_level = 1
+
+# Enemy related
+var enemies_close = []
+
+func _ready() -> void:
+	attack()
+
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction.x > 0: sprite_2d.flip_h = true
@@ -20,9 +39,43 @@ func _physics_process(delta: float) -> void:
 	velocity = direction * movement_speed
 	move_and_collide(velocity * delta)
 	
-
-
 func _on_hurtbox_hurt(damage: Variant) -> void:
 	hp -=damage
 	print("hp ", hp)
-	
+
+func attack():
+	if ice_spear_level > 0:
+		ice_spear_timer.wait_time = ice_spear_attack_speed
+		if ice_spear_timer.is_stopped():
+			ice_spear_timer.start()
+
+func _on_ice_spear_timer_timeout() -> void:
+	ice_spear_ammo += ice_spear_base_ammo
+	ice_spear_attack_timer.start()
+
+func _on_ice_spear_attack_timer_timeout() -> void:
+	if ice_spear_ammo > 0:
+		var ice_spear_attack = ice_spear.instantiate()
+		ice_spear_attack.position = position
+		ice_spear_attack.target = get_random_target()
+		ice_spear_attack.level = ice_spear_level
+		add_child(ice_spear_attack)
+		ice_spear_ammo -=1
+		if ice_spear_ammo > 0:
+			ice_spear_attack_timer.start()
+		else:
+			ice_spear_attack_timer.stop()
+
+func get_random_target()->Vector2:
+	if enemies_close.size() > 0:
+		return enemies_close.pick_random().global_position
+	return Vector2.UP
+
+func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
+	if not enemies_close.has(body): 
+		enemies_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
+	if enemies_close.has(body): 
+		enemies_close.erase(body)
