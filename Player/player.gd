864b7f52f75ebcbb47e0eaba1e87902b6139aee8
Logan Croley <logan.croley.18@gmail.com>
2024-09-12 13:09:48 -0400

    added tornado attack

--- Player/player.gd
+++ Player/player.gd
@@ -2,21 +2,33 @@ extends CharacterBody2D
 
 @export var movement_speed = 40.0
 @export var hp = 80
+var last_movement = Vector2.UP
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var walk_cycle_timer: Timer = $WalkCycleTimer
 
 # Attacks
 var ice_spear = preload("res://Player/Attacks/IceSpear/ice_spear.tscn")
+var tornado = preload("res://Player/Attacks/Tornado/tornado.tscn")
 
 # AttackNodes
-@onready var ice_spear_timer:Timer = get_node("%IceSpearTimer")
-@onready var ice_spear_attack_timer:Timer = get_node("%IceSpearAttackTimer")
+@onready var ice_spear_timer:Timer = %IceSpearTimer
+@onready var ice_spear_attack_timer:Timer = %IceSpearAttackTimer
+@onready var tornado_timer: Timer = %TornadoTimer
+@onready var tornado_attack_timer: Timer = %TornadoAttackTimer
 
 # IceSpear
-var ice_spear_ammo = 0
-var ice_spear_base_ammo = 1
-var ice_spear_attack_speed = 1.5
-var ice_spear_level = 1
+@export_group("IceSpear")
+@export var ice_spear_base_ammo = 1
+@export var ice_spear_ammo = 0
+@export var ice_spear_attack_speed = 1.5
+@export var ice_spear_level = 1
+
+# Tornado
+@export_group("Tornado")
+@export var tornado_ammo = 0
+@export var tornado_base_ammo = 1
+@export var tornado_attack_speed = 3
+@export var tornado_level = 1
 
 # Enemy related
 var enemies_close = []
@@ -30,6 +42,8 @@ func _physics_process(delta: float) -> void:
 	elif direction.x < 0: sprite_2d.flip_h = false
 	
 	if direction != Vector2.ZERO:
+		last_movement = direction
+		print(last_movement)
 		if walk_cycle_timer.is_stopped():
 			if sprite_2d.frame >=sprite_2d.hframes -1:
 				sprite_2d.frame = 0
@@ -48,6 +62,10 @@ func attack():
 		ice_spear_timer.wait_time = ice_spear_attack_speed
 		if ice_spear_timer.is_stopped():
 			ice_spear_timer.start()
+	if tornado_level > 0:
+		tornado_timer.wait_time = tornado_attack_speed
+		if tornado_timer.is_stopped():
+			tornado_timer.start()
 
 func _on_ice_spear_timer_timeout() -> void:
 	ice_spear_ammo += ice_spear_base_ammo
@@ -65,6 +83,23 @@ func _on_ice_spear_attack_timer_timeout() -> void:
 			ice_spear_attack_timer.start()
 		else:
 			ice_spear_attack_timer.stop()
+
+func _on_tornado_timer_timeout() -> void:
+	tornado_ammo += tornado_base_ammo
+	tornado_attack_timer.start()
+
+func _on_tornado_attack_timer_timeout() -> void:
+	if tornado_ammo > 0:
+		var tornado_attack:Tornado = tornado.instantiate()
+		tornado_attack.position = position
+		tornado_attack.last_movement = last_movement
+		tornado_attack.level = tornado_level
+		add_child(tornado_attack)
+		tornado_ammo -=1
+		if tornado_ammo > 0:
+			tornado_attack_timer.start()
+		else:
+			tornado_attack_timer.stop()
 
 func get_random_target()->Vector2:
 	if enemies_close.size() > 0:
